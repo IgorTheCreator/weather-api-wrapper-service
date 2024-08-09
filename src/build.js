8afd97372fd943e78911d277a7e5d145f74b31c7
Morozov Igor <morozovigorq@gmail.com>
2024-08-09 19:10:45 +0300

    feat: add redis plugin

--- src/build.js
+++ src/build.js
@@ -1,7 +1,9 @@
 import Fastify from 'fastify'
+import redisPlugin from './plugins/redis-data-source.js'
 import 'dotenv/config'
 
-export async function build (options) {
+export async function build(options) {
   const app = Fastify(options)
+  app.register(redisPlugin)
   return app
 }
---
+++ src/plugins/redis-data-source.js
@@ -0,0 +1,11 @@
+import fp from 'fastify-plugin'
+import fastifyRedis from '@fastify/redis'
+
+async function redisPlugin(fastify, options) {
+  fastify.register(fastifyRedis, {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  })
+}
+
+export default fp(redisPlugin)
