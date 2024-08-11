6322829da4950e60a5f87d801f91e00820211b4f
Morozov Igor <morozovigorq@gmail.com>
2024-08-11 19:10:12 +0300

    fix: moved cors options to a separate file

--- src/plugins/cors.js
+++ src/plugins/cors.js
@@ -1,10 +1,8 @@
 import fp from 'fastify-plugin'
 import fastifyCors from '@fastify/cors'
 
-async function corsPlugin(fastify, options) {
-  fastify.register(fastifyCors, {
-    origin: false,
-  })
+async function corsPlugin (fastify, options) {
+  fastify.register(fastifyCors, options)
 }
 
 export default fp(corsPlugin, { name: 'cors-plugin' })
